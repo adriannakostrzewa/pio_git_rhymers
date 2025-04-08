a567d2f8fda524629d09a249a34f91b57a7feefc
Oskar <251250@edu.p.lodz.pl>
2025-04-08 19:53:38 +0200

    6. ustawienie stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,37 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private static final int STACK_CAPACITY = 12;
+    private static final int EMPTY_STACK_VALUE = -1;
+    private static final int FULL_STACK_INDEX = STACK_CAPACITY - 1;
 
-    public int total = -1;
+    private int[] numbers = new int[STACK_CAPACITY];
+
+    private int total = EMPTY_STACK_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_STACK_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_STACK_INDEX;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return NUMBERS[total];
+            return EMPTY_STACK_VALUE;
+        return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
-        return NUMBERS[total--];
+            return EMPTY_STACK_VALUE;
+        return numbers[total--];
     }
 
 }
